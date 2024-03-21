b67733443bfb994571e46693b11de15b0e5c2339
raihanonad <raihansetiawan3296@gmail.com>
2024-03-21 15:45:45 +0700

    pp

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@
 const {
   Model
 } = require('sequelize');
+const bcrypt = require("bcryptjs")
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -11,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      User.belongsTo(models.Profile)
+      User.hasOne(models.Profile)
       User.belongsToMany(models.Product,{
         through : "Transactions",
         foreignKey : "UserTransactionId"
