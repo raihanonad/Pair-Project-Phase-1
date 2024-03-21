24d8e80d54cf2e31449398fe454e51530d55ccae
arthayogi <arthayo91@gmail.com>
2024-03-21 12:03:27 +0700

    pp

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -6,6 +6,22 @@ class UserController {
     static postRegister(req, res) {
         
     }
+
+    static async test(req,res) {
+        try {
+            res.render('login')
+        } catch (error) {
+            throw error
+        }
+    }
+
+    static async testHome(req,res) {
+        try {
+            res.render('home')
+        } catch (error) {
+            throw error
+        }
+    }
 }
 
 module.exports = UserController
