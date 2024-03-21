90cd36eccda664d252cf3ef650328f527895b64b
arthayogi <arthayo91@gmail.com>
2024-03-22 00:41:09 +0700

    final?

--- migrations/20240321145320-add-product-stock.js
+++ migrations/20240321145320-add-product-stock.js
@@ -10,7 +10,7 @@ module.exports = {
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
     await queryInterface.addColumn('Products', 'stock', {
-      type: Sequelize.DataTypes.STRING
+      type: Sequelize.DataTypes.INTEGER
     })
   },
 
